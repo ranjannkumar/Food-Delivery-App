572330ca795ce43f07235e14d80e744e6299bffa
Ranjan kumar <ranjankumar5673@gmail.com>
2024-07-18 22:29:14 +0530

    creating add items section

---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Orders.css'
+import { assets } from '../../assets/assets'
+
+const Orders = () => {
+  return (
+    <div>
+      Orders
+    </div>
+  )
+}
+
+export default Orders
