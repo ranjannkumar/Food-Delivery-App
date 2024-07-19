cbe1b131dbac7e7634a81b88c0d4c5643d19e06e
Ranjan kumar <ranjankumar5673@gmail.com>
2024-07-19 10:45:00 +0530

    add items section created

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,10 +5,13 @@ import Sidebar from './components/Sidebar/Sidebar'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Navbar/>
       <hr />
       <div className="app-content">
