e12c095ad750a4daf310a506441d3707947f362e
Ranjan kumar <ranjankumar5673@gmail.com>
2024-07-21 20:24:35 +0530

    user order page created

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -75,8 +75,17 @@ const placeOrder = async(req,res) =>{
     console.log(error);
     res.json({success:false,message:"Error"})
   }
-
  }
+ //user orders for frontend
+  const userOrders = async(req,res)=>{
+    try {
+      const orders = await orderModel.find({userId:req.body.userId});
+      res.json({success:true,data:orders})
+    } catch (error) {
+      console.log(error);
+      res.json({success:false,message:"Error"})
+    }
 
+  }
 
-export {placeOrder,verifyOrder}
+export {placeOrder,verifyOrder,userOrders}
