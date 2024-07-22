3fc67090c2390faef6345d3b9cbad4e2283743d4
Ranjan kumar <ranjankumar5673@gmail.com>
2024-07-22 12:33:42 +0530

    order update feature added

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -19,9 +19,22 @@ const Orders = ({url}) => {
       toast.error("Error")
     }
   } 
-   useEffect(()=>{
+
+  const statusHandler = async(event,orderId) =>{
+    const response = await axios.post(url+"/api/order/status",{
+      orderId,
+      status:event.target.value
+    })
+    if (response.data.success) {
+      await fetchAllOrders();
+    }
+
+  }
+
+
+  useEffect(()=>{
     fetchAllOrders()
-   },[])
+  },[])
 
 
   return (
@@ -46,7 +59,7 @@ const Orders = ({url}) => {
                   }
                 </p>
                 <p className="order-item-name">
-                  {order.address.fistName+" "+order.address.lastName}
+                  {order.address.firstName+" "+order.address.lastName}
                 </p>
                 <div className="order-item-address">
                   <p>{order.address.street+","}</p>
@@ -56,7 +69,7 @@ const Orders = ({url}) => {
               </div>
               <p>Items : {order.items.length}</p>
               <p>${order.amount}</p>
-              <select>
+              <select onChange={(event)=>statusHandler(event,order._id)} value={order.status}>
                 <option value="Food Processing">Food Processing</option>
                 <option value="Out for delivery">Out for delivery</option>
                 <option value="Delivered">Delivered</option>
